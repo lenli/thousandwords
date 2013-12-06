47c4c6886ae87977c9b68a3aefce6ebdf177d1e7
Len Li <leonard.li@gmail.com>
2013-12-06 01:20:26 -0500

    Initial Commit

---
+++ ThousandWords/TWAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  TWAppDelegate.h
+//  ThousandWords
+//
+//  Created by Len on 12/6/13.
+//  Copyright (c) 2013 LL inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ ThousandWords/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ThousandWords
+//
+//  Created by Len on 12/6/13.
+//  Copyright (c) 2013 LL inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TWAppDelegate class]));
+    }
+}
